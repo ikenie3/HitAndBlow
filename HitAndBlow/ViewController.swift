9a257512f68b61bc3d21ee11b14347026cc95a2a
takemitsu <takemitsu@notespace.jp>
2017-08-21 18:47:59 +0900

    Initial Commit

---
+++ HitAndBlow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitAndBlow
+//
+//  Created by takemitsu on 2017/08/21.
+//  Copyright © 2017年 takemitsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
